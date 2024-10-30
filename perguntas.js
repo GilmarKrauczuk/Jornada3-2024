9388f73d86a0e5f2af58bfc275d70383553fd764
GilmarKrauczuk <124721427+GilmarKrauczuk@users.noreply.github.com>
2024-10-30 22:27:03 +0000

    js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma linguagem de marcação.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é uma progressão aritmética (PA)?',
+    'PA é uma sequência de termos em que a diferença entre um termo e o seu anterior é sempre constante.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma linguagem de marcação.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma linguagem de marcação.'
+)
